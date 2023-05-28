8c696247824464c886164de7f72841c4a937e0a0
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-28 21:18:07 +0700

    Update Skull.py

--- Skull.py
+++ Skull.py
@@ -942,10 +942,10 @@ def banner():
 	cetak(panel(f"""\t{asu}
 
    [bold cyan]_  _________  ___  _____   __  _______  __ ™
-[bold cyan]  | |/_/ ___/\ \/ / |/ / _ | /  |/  / __ \/ /     AOUTHOR : BRAYENNNXD AND NOLEPXX 
-[bold cyan] _>  </ /__   \  /    / __ |/ /|_/ / /_/ / /__    Version: 2.6
+[bold cyan]  | |/_/ ___/\ \/ / |/ / _ | /  |/  / __ \/ /    AOUTHOR : BRAYENNNXD AND NOLEPXX 
+[bold cyan] _>  </ /__   \  /    / __ |/ /|_/ / /_/ / /__   Version: 2.6
 [bold cyan]/_/|_|\___/   /_/_/|_/_/ |_/_/  /_/\____/____/
-               {m}▪︎{k}▪︎{h}▪︎{sir} XCYNAMOL XXX BUFFALLO BILL {x}{m}▪︎{k}▪︎{h}▪︎{x}                                                                                   
+         {m}▪︎{k}▪︎{h}▪︎{sir} XCYNAMOL XXX BUFFALLO BILL {x}{m}▪︎{k}▪︎{h}▪︎{x}                                                                                   
              """,width=90,title=f"Banner",style=f"bold cyan"))
 #--------------------[ BAGIAN-MASUK ]--------------#
 def login():
