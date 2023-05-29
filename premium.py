5eca306f97a016cef8fcf5b67ca6c0f20bd81537
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-29 17:42:34 +0700

    Update premium.py

--- premium.py
+++ premium.py
@@ -71,7 +71,7 @@ try:
 	proxylist= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
 	open('socksku.txt','w').write(proxylist)
 except Exception as e:
-	bra_anim(f'gagal ster :(')
+	print(f'gagal ster :(')
 proxsi=open('socksku.txt','r').read().splitlines()
 
 ###----------[ USER AGENT ]----------###
