789302ce0cd00fc7b9b49b9844e6c0aea2a309ad
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-29 17:38:32 +0700

    Update premium.py

--- premium.py
+++ premium.py
@@ -66,14 +66,15 @@ princp=[]
 from rich.console import Console
 from rich.columns import Columns
 wa = Console()
-#------------------[ USER-AGENT ]-------------------#
+###----------[ GET PROXY ]----------###
 try:
 	proxylist= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
-	open('.pxy.txt','w').write(proxylist)
+	open('socksku.txt','w').write(proxylist)
 except Exception as e:
-	print(' [+] Koneksi Internet Anda Tidak Terdeteksi Silahkan Cek Kuota Anda Ya Salam Dari BrayennnXD');exit()
-proxsi=open('.pxy.txt','r').read().splitlines()
-####-----------User agen--------###
+	bra_anim(f'gagal ster :(')
+proxsi=open('socksku.txt','r').read().splitlines()
+
+###----------[ USER AGENT ]----------###
 for agenkuw in range(10000):
 	a='Mozilla/5.0 (Linux; Android'
 	b=random.choice(['8.1.0','9','10','11','12'])
