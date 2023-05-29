b9a2a878cfb7606c6db08985d5a3f239c5d85d43
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-29 14:14:34 +0700

    Update premium.py

--- premium.py
+++ premium.py
@@ -68,7 +68,7 @@ from rich.columns import Columns
 wa = Console()
 #------------------[ USER-AGENT ]-------------------#
 try:
-	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all').text
 	open('.socksku.txt','w').write(prox)
 except Exception as e:
 	print(' [+] Koneksi Internet Anda Tidak Terdeteksi Silahkan Cek Kuota Anda Ya Salam Dari BrayennnXD');exit()
